f7a9047836258e7ff79befc0367f792903073d08
uhasler <ulrich.hasler@st.ovgu.de>
2018-01-26 23:30:22 +0100

    fggf

--- Assets/Scripts/WeltGenerierung.cs
+++ Assets/Scripts/WeltGenerierung.cs
@@ -7,13 +7,15 @@ public class WeltGenerierung : MonoBehaviour {
 
     public int x = 10, y = 10;
     public int mineMax = 10;
+    public Texture2D bild;
     public GameObject[,] spielfeld;
+    public Material materi;
 	// Use this for initialization
 	void Start () {
         //Weltgenerierung
         spielfeld = new GameObject[x,y];
-        Camera camera = GetComponent<Camera>();
-        
+       // Camera camera = GetComponent<Camera>();
+      
         for (int i=0; i < x; i++)
         {
             for(int j = 0; j < y; j++)
@@ -21,8 +23,13 @@ public class WeltGenerierung : MonoBehaviour {
                 GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 cube.AddComponent<Feldwerte>();
                 cube.AddComponent<GUIText>();
-                //cube.AddComponent<Mat>
-                
+                //cube.GetComponent<Renderer>().material.mainTexture=bild;
+                cube.GetComponent<MeshRenderer>().material=materi;
+                materi.mainTexture = bild;
+                cube.transform.localScale = new Vector3(15, 15, 1);
+
+
+
 
                 cube.transform.position = new Vector3((float)(i * 15)+7.5f-x*7.5f,(float)(j*15)+7.5f-y*7.5f,0);
 
