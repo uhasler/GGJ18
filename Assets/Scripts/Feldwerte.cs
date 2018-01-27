93626a88458326adf135aa1e9a9fec22ca5356bd
uhasler <ulrich.hasler@st.ovgu.de>
2018-01-27 13:57:52 +0100

    dsgtjkr

--- Assets/Scripts/Feldwerte.cs
+++ Assets/Scripts/Feldwerte.cs
@@ -3,16 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Feldwerte : MonoBehaviour {
-    bool ismine=false;
-    bool isflagged = false;
-    int nachbarn = 0;
+  public  bool ismine=false;
+  public  bool isflagged = false;
+  public  int nachbarn = 0;
+    Texture2D bildf;
+    Texture2D bildnf;
+    Texture2D bildm;
+    Texture2D bild0;
+    Texture2D bild1;
+    Texture2D bild2;
+    Texture2D bild3;
+    Texture2D bild4;
+    Texture2D bild5;
+    Texture2D bild6;
+    Texture2D bild7;
+    Texture2D bild8;
+   public int x, y;
+
+    
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))//rechts
+        {
+            isflagged = !isflagged;
+
+            if (isflagged)
+            {
+                GetComponent<MeshRenderer>().material.mainTexture = bildf;
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().material.mainTexture = bildnf;
+            }
+        }
+        if (Input.GetMouseButtonDown(0))//links
+        {
+            if (ismine&&!isflagged)
+            {
+                GetComponent<MeshRenderer>().material.mainTexture = bildm;
+            }
+            else if(!ismine)
+            {
+                switch (nachbarn)
+                {
+                    case 0: GetComponent<MeshRenderer>().material.mainTexture = bild0;
+                            
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
--- Assets/Scripts/WeltGenerierung.cs
+++ Assets/Scripts/WeltGenerierung.cs
@@ -8,10 +8,18 @@ public class WeltGenerierung : MonoBehaviour {
     public int x = 10, y = 10;
     public int mineMax = 10;
     public Texture2D bild;
-    public Texture2D bild1;
+    public Texture2D bildflagged;
     public Texture2D bild2;
     public Texture2D bild3;
     public Texture2D bild4;
+    public Texture2D bild5;
+    public Texture2D bild6;
+    public Texture2D bild7;
+    public Texture2D bild8;
+    public Texture2D bild1;
+    public Texture2D bild0;
+    public Texture2D bildnf;
+    public Texture2D bildm;
 
     public GameObject[,] spielfeld;
     public Material materi;
@@ -32,20 +40,113 @@ public class WeltGenerierung : MonoBehaviour {
                 cube.GetComponent<MeshRenderer>().material=materi;
                 materi.mainTexture = bild;
                 cube.transform.localScale = new Vector3(15, 15, 1);
-
+                cube.GetComponent<Feldwerte>().x = i;
+                cube.GetComponent<Feldwerte>().y = j;
 
 
 
                 cube.transform.position = new Vector3((float)(i * 15)+7.5f-x*7.5f,(float)(j*15)+7.5f-y*7.5f,0);
-
+                //cube.
                 spielfeld[i, j] = cube;
+
             }
         }
         
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    
+    
+     
+    /*void NachbarnAufdecken(int xp, int yp)
+    {
+        if (xp - 1 >= 0)
+        {
+            spielfeld[xp - 1, yp].GetComponent<Feldwerte>().nachbarn;
+        }
+        if (xp + 1 <= x)
+        {
+            spielfeld[xp + 1, yp].GetComponent<Feldwerte>().nachbarn;
+        }
+        if (yp - 1 >= 0)
+        {
+            spielfeld[xp, yp - 1].GetComponent<Feldwerte>().nachbarn;
+        }
+        if (yp + 1 <= y)
+        {
+            spielfeld[xp, yp + 1].GetComponent<Feldwerte>().nachbarn;
+        }
+    }*/
+    public int NachbarnErmitteln(int xp, int yp)
+    {
+        int ret = 0;
+
+       
+        
+            if (xp - 1 >= 0)
+            {
+                if (spielfeld[xp - 1, yp].GetComponent<Feldwerte>().ismine)
+                {
+                    ret++;
+                }
+            }
+            if (xp + 1 <= x)
+            {
+                if (spielfeld[xp + 1, yp].GetComponent<Feldwerte>().ismine)
+                {
+                    ret++;
+                }
+            }
+            if (yp - 1 >= 0)
+            {
+                if (spielfeld[xp , yp-1].GetComponent<Feldwerte>().ismine)
+                {
+                    ret++;
+                }
+            }
+            if (yp + 1 <= y)
+            {
+                if (spielfeld[xp , yp+1].GetComponent<Feldwerte>().ismine)
+                {
+                    ret++;
+                }
+            }
+            if (xp - 1 >= 0&& yp - 1 >= 0)
+            {
+                if (spielfeld[xp - 1, yp-1].GetComponent<Feldwerte>().ismine)
+                {
+                    ret++;
+                }
+            }
+            if (xp + 1 <= x&& yp + 1 >= y)
+            {
+                if (spielfeld[xp + 1, yp+1].GetComponent<Feldwerte>().ismine)
+                {
+                    ret++;
+                }
+            }
+            if (yp - 1 >= 0&& xp + 1 <= x)
+            {
+                if (spielfeld[xp+1, yp - 1].GetComponent<Feldwerte>().ismine)
+                {
+                    ret++;
+                }
+            }
+            if (yp + 1 <= y&& xp - 1 >= 0)
+            {
+                if (spielfeld[xp-1, yp + 1].GetComponent<Feldwerte>().ismine)
+                {
+                    ret++;
+                }
+            }
+
+
+        
+
+
+        return ret;
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
